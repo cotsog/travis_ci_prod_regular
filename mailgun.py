0277d18151ef2cec7dcc451f7f323aebec2c7b75
Dominic Jodoin <dominic@travis-ci.com>
2015-11-27 16:15:46 -0500

    print status_code

--- mailgun.py
+++ mailgun.py
@@ -4,7 +4,7 @@ import requests
 mailgun_credentials = os.environ['MAILGUN_CREDENTIALS']
 
 def send_simple_message():
-    return requests.post(
+    r = requests.post(
         "https://api.mailgun.net/v3/sandbox400a0020e1964c79bbf9202767651c72.mailgun.org/messages",
         auth=tuple(mailgun_credentials.split(':', 1)),
         data={"from": "Excited User <mailgun@sandbox400a0020e1964c79bbf9202767651c72.mailgun.org>",
@@ -12,5 +12,6 @@ def send_simple_message():
               "subject": "Hello",
               "text": "Testing some Mailgun awesomness!",
               "o:testmode": True})
+    print r.status_code
               
 send_simple_message()
