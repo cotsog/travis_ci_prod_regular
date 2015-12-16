90ff0fddaa210dcebc47fbb170b1361a5e68b1f4
Dominic Jodoin <dominic@travis-ci.com>
2015-12-15 22:26:59 -0500

    Update hostname.java

--- hostname.java
+++ hostname.java
@@ -2,6 +2,7 @@ import java.net.InetAddress;
 class Main {
   public static void main(String[] args) {
     try {
+    System.out.println( InetAddress.getLocalHost().getCanonicalHostName()  );
     System.out.println( InetAddress.getLocalHost().getHostName()  );
     } catch (Exception e) {
       System.out.println(e.getMessage());
