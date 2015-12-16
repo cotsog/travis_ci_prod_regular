42c4af463068ab7c51c271cd88954eb2ff6e0d62
Dominic Jodoin <dominic@travis-ci.com>
2015-12-15 23:56:55 -0500

    Update hostname.c

--- hostname.c
+++ hostname.c
@@ -5,35 +5,44 @@
 #include <string.h>
 
 int main(void) {
-    // Disable stdout buffering
-    setvbuf(stdout, NULL, _IONBF, 0);
-
-  char hostname[128];
-  struct addrinfo  hints, *res;
-  int error;
-  /* ensure null-terminated */
-  hostname[NI_MAXHOST] = '\0';
+    char hostname[NI_MAXHOST+1];
   
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_flags = AI_CANONNAME | AI_V4MAPPED;
-  hints.ai_family = (AF_INET);
+    hostname[0] = '\0';
+    if (gethostname(hostname, NI_MAXHOST)) {
+      /* Something went wrong, maybe networking is not setup? */
+      strcpy(hostname, "localhost");
+    } else {
+        /* This part is only kept for backwards compatibility. It can be enabled
+         * by setting the system property java.net.doIPv6ReverseLookupInGetLocalHost
+         * or java.net.doIPv6ReverseLookupInGetLocalHost to 'true'.
+         */
+        struct addrinfo  hints, *res;
+        int error;
+        /* ensure null-terminated */
+        hostname[NI_MAXHOST] = '\0';
   
-  error = getaddrinfo(hostname, NULL, &hints, &res);
-  printf("test %d", error);
-  if (error == 0) {
-    /* host is known to name service */
-    error = getnameinfo(res->ai_addr,
-                        res->ai_addrlen,
-                        hostname,
-                        NI_MAXHOST,
-                        NULL,
-                        0,
-                        NI_NAMEREQD);
-  }
-    /* if getnameinfo fails hostname is still the value
-       from gethostname */
+        memset(&hints, 0, sizeof(hints));
+        hints.ai_flags = AI_CANONNAME | AI_V4MAPPED;
+        hints.ai_family = AF_INET;
   
-    freeaddrinfo(res);
-    printf("Hostname: %s", hostname);
+        error = getaddrinfo(hostname, NULL, &hints, &res);
+  
+        if (error == 0) {
+          /* host is known to name service */
+          error = getnameinfo(res->ai_addr,
+                              res->ai_addrlen,
+                              hostname,
+                              NI_MAXHOST,
+                              NULL,
+                              0,
+                              NI_NAMEREQD);
+  
+          /* if getnameinfo fails hostname is still the value
+             from gethostname */
+  
+          freeaddrinfo(res);
+        }
+    }
+    printf("Hostname: %s\n", hostname);
     return 0;
-}
+  }
