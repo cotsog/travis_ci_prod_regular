026492f847ce756a7286389f292afd8daf8f17f6
Dominic Jodoin <dominic@travis-ci.com>
2016-05-30 16:00:36 -0400

    learning expect

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect --
 
 puts "BEGIN!"
-puts "$MAILGUN_CREDENTIALS"
-set mailgun_credentials $MAILGUN_CREDENTIALS
+puts $::env(MAILGUN_CREDENTIALS)
+set mailgun_credentials $::env(MAILGUN_CREDENTIALS)
 puts "$mailgun_credentials"
