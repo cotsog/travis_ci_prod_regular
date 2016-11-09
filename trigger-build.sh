62200434895003c07b73b468c7489cac9e397dfe
Dominic Jodoin <dominic@travis-ci.com>
2016-11-09 14:14:00 -0500

    Update trigger-build.sh

--- trigger-build.sh
+++ trigger-build.sh
@@ -7,6 +7,6 @@ curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
   -H "Travis-API-Version: 3" \
-  -H "Authorization: token 8WxtFuNRSpPE2snD0F3_Zg" \
+  -H "Authorization: token super-secret" \
   -d "$body" \
   https://api.travis-ci.org/repo/cotsog%2Ftravis_ci_prod_regular/requests
