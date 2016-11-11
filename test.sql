a2be48c8a464d52554345641e1792122585d6ec7
Dominic Jodoin <dominic@travis-ci.com>
2016-11-11 00:10:47 -0500

    fixed sql

--- test.sql
+++ test.sql
@@ -1,1 +1,6 @@
-SHOW TABLES;
+CREATE TABLE article (
+    article_id bigserial primary key,
+    article_name varchar(20) NOT NULL,
+    article_desc text NOT NULL,
+    date_added timestamp default NULL
+);
